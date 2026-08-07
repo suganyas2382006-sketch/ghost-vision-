55fd8ac16503b04d4d014f381503ff164cec23d5
suganyas2382006-sketch <suganyas2382006@gmail.com>
2026-08-07 13:45:11 +0530

    Create src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+GhostVision Source Package
+Contains modules for gesture recognition, human segmentation, and visual effects.
+"""
+
+# Assuming you will create classes or main functions in your modules, 
+# you can expose them here for cleaner imports in main.py.
+
+# from .gesture_recognition import GestureTracker
+# from .segmentation import BackgroundSubtractor, HumanSegmenter
+# from .visual_effects import apply_invisibility, apply_ghost_mode, take_screenshot
+
+__version__ = "1.0.0"
+__author__ = "GhostVision Developer"
