c9781a1090b1e888c1446de8854b064e16bf3268
suganyas2382006-sketch <suganyas2382006@gmail.com>
2026-08-24 13:41:49 +0530

    Create backend/src/__init__.py

---
+++ backend/src/__init__.py
@@ -0,0 +1,11 @@
+"""
+GhostVision Source Package
+Contains modules for gesture recognition, human segmentation, and visual effects.
+"""
+
+from .gesture_recognition import GestureTracker
+from .segmentation import HumanSegmenter
+from .visual_effects import apply_invisibility
+
+__version__ = "1.0.0"
+__author__ = "GhostVision Developer"
